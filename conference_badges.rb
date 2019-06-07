2f21f66647d3b001c0b80048cd4774375ed3034a
Trejon Stallsworth <trejonstallsworth@gmail.com>
2019-06-07 17:15:24 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -6,3 +6,7 @@ def badge_maker(name)
  def batch_badge_creator(names)
    return names.map {|badge| badge_maker(badge)}
  end
+ 
+ def assign_rooms(attendees)
+   attendees.each_with_index.map{|name, index| "Hello, #{name}! You'll be assigned to room #{index+1}!"}
+ end 
